6f51ad68a4bd738485945a55141cb8cf96caf53d
irof <irof.ocean@gmail.com>
2026-04-21 23:59:30 +0900

    update

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <new_version> <jig_version>"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <new_version>"
   exit 1
 fi
 
 NEW_VERSION=$1
-JIG_VERSION=$2
+JIG_VERSION=$1
 
 echo "Updating versions in pom.xml..."
 sed -i '' "4s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom.xml
@@ -17,10 +17,3 @@ echo "Updating versions in pom-irof.xml..."
 sed -i '' "5s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom-irof.xml
 sed -i '' "17s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom-irof.xml
 
-echo "Running mvn clean deploy for pom.xml..."
-./mvnw clean deploy -DskipTests
-
-echo "Running mvn clean deploy for pom-irof.xml..."
-./mvnw -f pom-irof.xml clean deploy -DskipTests
-
-echo "Release complete!"
