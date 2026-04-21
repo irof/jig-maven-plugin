cbab330fe14f43065fdec28248a4649c0426cb53
irof <irof.ocean@gmail.com>
2026-04-22 00:04:31 +0900

    Fix line numbers in update.sh

--- update.sh
+++ update.sh
@@ -10,10 +10,10 @@ NEW_VERSION=$1
 JIG_VERSION=$1
 
 echo "Updating versions in pom.xml..."
-sed -i '' "4s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom.xml
-sed -i '' "24s|<version>.*</version>|<version>${JIG_VERSION}</version>|" pom.xml
+sed -i '' "6s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom.xml
+sed -i '' "25s|<version>.*</version>|<version>${JIG_VERSION}</version>|" pom.xml
 
 echo "Updating versions in pom-irof.xml..."
-sed -i '' "5s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom-irof.xml
+sed -i '' "6s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom-irof.xml
 sed -i '' "17s|<version>.*</version>|<version>${NEW_VERSION}</version>|" pom-irof.xml
 
